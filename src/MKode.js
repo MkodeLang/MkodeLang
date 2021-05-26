9dc4a6b7196cad08e6d48bcd2b4f1c8769c3e519
Rak Laptudirm <68542775+raklaptudirm@users.noreply.github.com>
2021-05-26 16:02:34 +0530

    chore: Added `.gitignore`

--- src/MKode.js
+++ src/MKode.js
@@ -1,8 +1,7 @@
 import Lexer from "./Lexer.js"
+import fs from "fs"
 
-const source =
-  "const func = (a, b) => a + b;\n# this is comment\nconst PI = 3.141592; #wow comment"
-  //"\"hello\"\n38435.3824385"
+const source = fs.readFileSync("../test.kode").toString()
 const code = new Lexer(source)
 
 console.log(code.lex())
